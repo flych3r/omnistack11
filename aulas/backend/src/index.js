cb7141afb5a6e0e17b222135f562b093ed4b5c52
flycher <xavier_sampaio@alu.ufc.br>
2020-03-25 21:24:33 -0300

    frontend

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -4,7 +4,10 @@ const routes = require('./routes')
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+  exposedHeaders: 'Authorization',
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(routes)
 
